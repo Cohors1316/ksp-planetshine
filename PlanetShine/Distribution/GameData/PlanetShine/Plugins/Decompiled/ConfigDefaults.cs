d47d41512b1ac7ab2dd2d1f0a3da73cd42af59f7
Ben Jordan <ben@poecoh.com>
2021-02-14 09:12:56 -0500

    Create ConfigDefaults.cs

---
+++ PlanetShine/Distribution/GameData/PlanetShine/Plugins/Decompiled/ConfigDefaults.cs
@@ -0,0 +1,19 @@
+namespace PlanetShine
+{
+  public class ConfigDefaults
+  {
+    public static float baseAlbedoIntensity = 0.24f;
+    public static float vacuumLightLevel = 0.03f;
+    public static float baseGroundAmbient = 0.6f;
+    public static float groundAmbientOverrideRatio = 0.6f;
+    public static float minAlbedoFadeAltitude = 0.02f;
+    public static float maxAlbedoFadeAltitude = 0.1f;
+    public static float minAmbientFadeAltitude = 0.0f;
+    public static float maxAmbientFadeAltitude = 0.1f;
+    public static float albedoRange = 10f;
+
+    private ConfigDefaults()
+    {
+    }
+  }
+}
