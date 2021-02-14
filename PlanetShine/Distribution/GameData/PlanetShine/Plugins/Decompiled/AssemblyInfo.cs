97715e3ad03c438d1e307c663f77385f3a4f3551
Ben Jordan <ben@poecoh.com>
2021-02-14 09:08:35 -0500

    Create AssemblyInfo.cs

---
+++ PlanetShine/Distribution/GameData/PlanetShine/Plugins/Decompiled/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("PlanetShine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Valerian")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyFileVersion("0.2.6.1")]
+[assembly: AssemblyVersion("0.2.6.1")]
